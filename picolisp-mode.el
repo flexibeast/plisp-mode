40e955d64d8ae2e8455deeaed4aef6a8c0b525a4
Alexis <flexibeast@gmail.com>
2019-08-01 20:03:46 +1000

    Update copyright year range.

--- picolisp-mode.el
+++ picolisp-mode.el
@@ -1,6 +1,6 @@
 ;;; picolisp-mode.el --- Major mode for PicoLisp programming.
 
-;; Copyright (C) 2014-2015  Alexis <flexibeast@gmail.com>
+;; Copyright (C) 2014-2019  Alexis <flexibeast@gmail.com>
 
 ;; Author: Alexis <flexibeast@gmail.com>
 ;; Maintainer: Alexis <flexibeast@gmail.com>
